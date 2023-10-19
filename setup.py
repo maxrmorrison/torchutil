afd8a6070a9e0a03cb71d6831c320c666f68d036
Max Morrison <maxrmorrison@gmail.com>
2023-10-19 15:22:58 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as file:
 setup(
     name='torchutil',
     description='PyTorch utilities for developing deep learning frameworks',
-    version='0.0.1',
+    version='0.0.2',
     author='Max Morrison',
     author_email='maxrmorrison@gmail.com',
     url='https://github.com/maxrmorrison/torchutil',
