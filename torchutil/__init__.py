9d48c9ac82e52fa5286d6c3ee1f40e603fa1eeac
Max <maxrmorrison@gmail.com>
2023-12-31 21:00:40 -0600

    Inference context import

--- torchutil/__init__.py
+++ torchutil/__init__.py
@@ -2,6 +2,7 @@ from . import checkpoint
 from . import cuda
 from . import download
 from . import gradients
+from . import inference
 from . import metrics
 from . import paths
 from . import tensorboard
